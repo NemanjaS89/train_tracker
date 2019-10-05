bfcfda453795ff5be22fcd906d947854781975b3
NemanjaS89 <simic.nemanja1989@gmail.com>
2019-10-05 10:51:58 +0200

    final

--- train_tracker_hr.py
+++ train_tracker_hr.py
@@ -17,20 +17,30 @@ sheet.cell_value(0, 0)
 
 #initiating the browser
 browser = webdriver.Chrome('resources/chromedriver.exe')
-browser.get('http://vred.hzinfra.hr/hzinfo/Default.asp?Category=hzinfo&Service=tpvl&SCREEN=1')
+browser.get('http://vred.hzinfra.hr/hzinfo/Default.asp?Category=hzinfo&Service=TPVG&SCREEN=1')
 
 for i in range (sheet.nrows):
-    text_field = browser.find_element_by_name('VL')
     
-    text_field.send_keys(sheet.cell_value(i, 0))
-    submit_button = browser.find_element_by_xpath("//input[@value=' OK ']")
+    text_field = browser.find_element_by_name('VAG')
+    
+    text_field.send_keys(str(sheet.cell_value(i, 0)))
+    
+    submit_button = browser.find_element_by_xpath("//input[@type='SUBMIT']")
     submit_button.click()
     
     soup = BeautifulSoup(browser.page_source, 'lxml')
-    result = soup.find_all('p')[2]
+    status = soup.find_all('td')[3]
+    location = soup.find_all('td')[4]
+    time = soup.find_all('td')[2]
     
-    worksheet.write(row, col, sheet.cell_value(i, 0))
-    worksheet.write(row, col + 1, result.text)
+    worksheet.write(row, col, str(sheet.cell_value(i, 0)))
+    worksheet.write(row, col + 1, status.text)
+    worksheet.write(row, col + 2, location.text)
+    worksheet.write(row, col + 3, time.text)
+    
+    back_button = browser.find_element_by_xpath("//input[@type='SUBMIT']")
+    back_button.click()
+    
     row += 1
     
 workbook.close()
